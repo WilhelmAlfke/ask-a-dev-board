b99cb4efc7e3578cb1f3387047b754cf4378a935
Stefan Fitoussi <stefan.fit@web.de>
2022-11-02 12:56:49 +0100

    fetch data

---
+++ pages/api/questions/index.js
@@ -0,0 +1,8 @@
+// import { getQuestions } from '../../../helpers/api'
+
+// function handler(request, response) {
+//   const questions = getQuestions()
+//   response.status(200).json(questions)
+// }
+
+// export default handler
